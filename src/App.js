6aa0cf88780bc520bc09080cb246d1da8efb0c54
Skorin Vladislav <43670004+Skorin-Vladislav@users.noreply.github.com>
2023-01-11 12:25:14 +0300

    header

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="header">
+        <h1>Reliable, efficient delivery</h1>
+        <h1>Powered by Technology</h1>
+        <p>
+          Our Artificial Intelligence powered tools use millions of project data
+          points to ensure that your project is successful
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default App;
